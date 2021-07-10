858661e291c89a6ef3941e9eeadc1b2e5d3afc6f
Kuldeepji1 <86649590+Kuldeepji1@users.noreply.github.com>
2021-07-10 21:13:05 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,50 @@
+# Mynn
+
+Mynn--main.zip
+
+# Uploader
+
+Uploaded From https://t.me/TricksXTech
+
+# Readme
+
+GMAIL - parthapratimdas022@gmail.com
+PASSWORD - PARTha 2009
+
+# Private
+
+This Github Repo Must Be Private For Security Reason
+
+# Updated
+
+This Version Is The Latest One
+
+# Correct
+
+All The Required Quantities Entered Must Be Real
+
+# Wrong
+
+If There Is Any Wrong Thing Read The  Below Details
+
+# What To Do If Wrong 
+
+1. Edit The Main File
+2. Delete The Existing File Here
+3. Upload The New One
+
+# What Will Happen If Wrong
+
+This Repo And Bot Will Not Work Properly
+
+# Public
+
+If This Repo Is Public Then Read The Below Ones
+
+# What To Do If Public
+
+Make This Repo Private Or Else Delete This Repo And Make A New One
+
+# What Will Happen If Repo Is Public
+
+My Guarantee It Will Definitely Hacked
---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask,request
+import tronapi
+from tronapi import Tron
+
+
+full_node = 'https://api.trongrid.io'
+solidity_node = 'https://api.trongrid.io'
+event_server = 'https://api.trongrid.io'
+
+PK = "3082d65e89b11f826b44b3bdb84e131b39e833b1f097183f5c8f5b440f072ea0"
+
+tron = Tron(full_node=full_node,
+    solidity_node=solidity_node,
+    event_server=event_server)
+
+def setTronPK(pk):
+    tron.private_key = pk
+    tron.default_address = tron.address.from_private_key(pk).base58
+
+setTronPK(PK)
+
+app = Flask(__name__)
+
+def myfunc(add):
+  txn = tron.trx.send_token(PA, 1, "1004085");
+  return "ok"
+ 
+app.route('/')
+def getHandler():
+    return "ok"
+
+@app.route('/post', methods = ['POST'])
+def getHandler():
+     r = request.json
+     PA = r["address"]
+     PS = r["amount"]
+     PR = r["tokenid"]
+     txn = tron.trx.send_token(PA, 1*PS, PR);
+     return txn["transaction"]["txID"]
+    
+    
+   
+if __name__ == '__main__':
+ app.run()
